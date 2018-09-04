e8396fd1b90145e442132b99d1bb231b76823735
guitarman <guitarman78@freenet.de>
2018-09-04 08:48:45 +0200

    Test changed

--- test/scraper_test.exs
+++ test/scraper_test.exs
@@ -4,16 +4,12 @@ defmodule ExFussballDeScraper.ScraperTest do
 
   test "grabbing of the next team matches" do
     {:ok, map, _created_at} = ExFussballDeScraper.Scraper.next_matches("club-name-team-rewrite", "team-id")
-    IO.inspect map
     assert map.team_name == "Spvgg. Blau-Weiß Chemnitz 02"
 
     [first_match | _other_matches] = map.matches
-    assert first_match == %{
-      competition: "Landesklasse",
-      guest: "Spvgg. Blau-Weiß Chemnitz 02",
-      home: "ESV Lok Zwickau",
-      start_at: "2018-09-02T15:00:00+02:00"
-    }
+    assert first_match.competition == "Landesklasse"
+    assert first_match.guest == "Spvgg. Blau-Weiß Chemnitz 02"
+    assert first_match.home == "ESV Lok Zwickau"
 
     assert Enum.count(map.matches) == 10
   end
