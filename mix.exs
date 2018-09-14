ebccd6f481e96ddf2ebbcec0424ba93247fcf54e
guitarman <guitarman78@freenet.de>
2018-09-14 20:15:45 +0200

    test fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExFussballDeScraper.MixProject do
   def project do
     [
       app: :ex_fussball_de_scraper,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env == :prod,
